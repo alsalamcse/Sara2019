2140a9a6e30946177b2adba36655c3d982e85beb
Owner <alsalamsce>
2019-09-19 14:07:32 +0300

    tdcuidlo

--- app/src/main/java/com/example/sarataskmng/Welcome.java
+++ app/src/main/java/com/example/sarataskmng/Welcome.java
@@ -2,6 +2,7 @@ package com.example.sarataskmng;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Welcome extends AppCompatActivity {
@@ -10,7 +11,24 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Thread th=new Thread() {
+            public void run() {
+                try {
+                    sleep(3 * 1000);
+                    Intent i=new Intent(getApplication(),SignIn.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                ;
+            }
+        };
+        th.start();
+
+
     }
 
-    //sdkldfweafg
+
 }
