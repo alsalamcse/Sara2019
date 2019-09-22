954764f545ccc250cab86cc7844cd65d5d64bc73
Owner <alsalamsce>
2019-09-22 14:07:32 +0300

    tdcuidloh

--- app/src/main/java/com/example/sarataskmng/SignIn.java
+++ app/src/main/java/com/example/sarataskmng/SignIn.java
@@ -3,6 +3,7 @@ package com.example.sarataskmng;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -11,8 +12,10 @@ public class SignIn extends AppCompatActivity {
 
     private ImageView img;
     private EditText tvEmail2 ;
+    private EditText etPass;
     private Button btnIn;
     private Button btnUp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,27 @@ public class SignIn extends AppCompatActivity {
 
         img=(ImageView)findViewById(R.id.img);
         tvEmail2=(EditText)findViewById(R.id.tvEmail2);
+        etPass=(EditText)findViewById(R.id.etPass);
         btnIn=(Button)findViewById(R.id.btnIn);
         btnUp=(Button)findViewById(R.id.btnUp);
 
+        btnIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datahandler();
+            }
+        });
+
     }
+
+
+
+    private void datahandler(){
+        String email=tvEmail2.getText().toString();
+        String passw=etPass.getText().toString();
+    }
+
+
+
+
 }
