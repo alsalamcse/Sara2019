fe0ba4b5d6c6aeba669207e50175f99092c4bf52
Owner <alsalamsce>
2019-10-27 12:22:18 +0200

    tdcuidloh

--- app/src/main/java/com/example/sarataskmng/ui/main/AllTaskFragment.java
+++ app/src/main/java/com/example/sarataskmng/ui/main/AllTaskFragment.java
@@ -3,16 +3,23 @@ package com.example.sarataskmng.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.sarataskmng.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import data.MyTask;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -31,12 +38,28 @@ public class AllTaskFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_all_task, container, false);
     }
+
     public void readTaskFromFirebase(){
         FirebaseDatabase database=FirebaseDatabase.getInstance();
         FirebaseAuth auth=FirebaseAuth.getInstance();
         String uid = auth.getUid();
         DatabaseReference reference = database.getReference();
-        
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot d: dataSnapshot.getChildren())
+                {
+                    MyTask t =d.getValue(MyTask.class);
+                    Log.d("MyTask",t.toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
