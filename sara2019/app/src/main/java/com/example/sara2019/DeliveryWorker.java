9fe69bb2218458ee76750ae61ee7111c921c98fc
Owner <alsalamsce>
2020-02-17 11:36:32 +0200

    get

--- sara2019/app/src/main/java/com/example/sara2019/DeliveryWorker.java
+++ sara2019/app/src/main/java/com/example/sara2019/DeliveryWorker.java
@@ -3,12 +3,21 @@ package com.example.sara2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DeliveryWorker extends AppCompatActivity {
+
+    private Button btnAccept;
+    private Button btnReject;
+    private Button btnWait;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_worker);
+
+        btnAccept=(Button)findViewById(R.id.btnAccept);
+        btnReject=(Button)findViewById(R.id.btnReject);
+        btnWait=(Button)findViewById(R.id.btnWait);
     }
 }
--- sara2019/app/src/main/java/com/example/sara2019/Order.java
+++ sara2019/app/src/main/java/com/example/sara2019/Order.java
@@ -3,12 +3,23 @@ package com.example.sara2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Order extends AppCompatActivity {
+
+    private EditText etLocat;
+    private EditText etDest;
+    private EditText etPhoneNum;
+    private EditText etOrderNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        etLocat=(EditText)findViewById(R.id.etLocat);
+        etDest=(EditText)findViewById(R.id.etDest);
+        etPhoneNum=(EditText)findViewById(R.id.etPhoneNum);
+        etOrderNum=(EditText)findViewById(R.id.etOrderNum);
     }
 }
